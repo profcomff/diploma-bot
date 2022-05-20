adfd01c7104b2191a7dc964a6d0808b08c3bb7bf
Mikhail <mmikee00800@gmail.com>
2022-05-20 18:29:20 +0300

    cache-only use + answer message

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 import requests
 import configparser
 from vk_api import VkApi
+from vk_api.utils import get_random_id
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from google_drive.google_drive_utils import upload_file_to_drive, get_service
 from google_drive.token import get_token_file
@@ -12,6 +13,7 @@ GROUP_ID = config['auth_vk']['group_id']
 GROUP_TOKEN = config['auth_vk']['group_token']
 DOWNLOAD_TAG = config['tags']['test_tag']
 DIPLOMA_FOLDER = config['drive_repo']['diploma_folder']
+ANSWER = config['ans']['onsuccess']
 API_VERSION = '5.131'
 
 vk_session = VkApi(token=GROUP_TOKEN, api_version=API_VERSION)  # Auth with community token
@@ -20,24 +22,24 @@ vk_api = vk_session.get_api()
 drive_service = get_service('token.json')
 
 
-def process_event(event, cache=True):
+def send_message(vk, user_id, message):
+    params = {'user_id': user_id, 'random_id': get_random_id(), 'message': message}
+    vk.method('messages.send', params)
+
+
+def process_event(event):
     if event.type == VkBotEventType.MESSAGE_NEW and event.from_user:
         if DOWNLOAD_TAG in (event.message['text']).lower() and event.message.attachments:
             title = event.message.attachments[0]['doc']['title']
             url = event.message.attachments[0]['doc']['url']
             content = requests.get(url).content
-            if cache:
-                fmt = title.split('.')[1]
-                file = f'cache.{fmt}'
-                with open(file, 'wb') as cachefile:
-                    cachefile.write(content)
-                print(f'File: {title} uploaded to cache')
-                upload_file_to_drive(drive_service, file=file, name=title, folder_name=DIPLOMA_FOLDER)
-            else:
-                with open(title, 'wb') as file:
-                    file.write(content)
-                print(f'File: {title} uploaded to server')
-                upload_file_to_drive(drive_service, title)
+            fmt = title.split('.')[1]
+            file = f'cache.{fmt}'
+            with open(file, 'wb') as cachefile:
+                cachefile.write(content)
+            print(f'File: {title} uploaded to cache')
+            upload_file_to_drive(drive_service, file=file, file_name=title, folder_name=DIPLOMA_FOLDER)
+            send_message(vk_session, event.message['from_id'], ANSWER)
 
 
 def chat_loop():
